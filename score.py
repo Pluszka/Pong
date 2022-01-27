45cc87e9534145157ddf191a7d56c997ed81e174
Pliszka <karoliniakkjk@gmail.com>
2022-01-27 23:20:38 +0100

    prompting score

--- score.py
+++ score.py
@@ -1,7 +1,19 @@
 from turtle import Turtle
 
-class Score:
+ALIGNMENT = 'center'
+FONT = ("Rockwell", 25, 'bold')
 
-    def __init__(self):
+class Score(Turtle):
+
+    def __init__(self, side):
         super().__init__()
         self.score = 0
+        self.hideturtle()
+        self.penup()
+        self.color('white')
+        self.goto(y=250, x=side)
+        self.prompt()
+
+    def prompt(self):
+        self.write(self.score, align=ALIGNMENT, font=FONT)
+
